c9864cd186c1bd4c78c01e1b65b4679a0e6bfa16
Sanjay Singh <sanjay.f.singh@oracle.com>
2026-01-13 21:39:27 +0530

    fixed build error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,35 +5,50 @@ export function Footer() {
     <footer>
       <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
         {/* <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"> */}
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2030{" "}
-            <Link href="/" className="hover:underline">
-              Cinemate
-            </Link>
-            . All Rights Reserved.
-          </span>
-          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">
-                Youtube
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank" className="hover:underline">
-                Github
-              </a>
-            </li>
-          </ul>
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2030{" "}
+          <Link href="/" className="hover:underline">
+            Cinemate
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a
+              href="https://www.instagram.com/sanju.inlv/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:underline me-4 md:me-6"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sanjuinlv/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:underline me-4 md:me-6"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sanjuinlv"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:underline me-4 md:me-6"
+            >
+              Youtube
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/sanjuinlv" target="_blank" rel="noreferrer" className="hover:underline">
+              Github
+            </a>
+          </li>
+        </ul>
         {/* </div> */}
       </footer>
     </footer>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,12 +9,12 @@ export function Search({ apiPath }) {
   console.log(`apiPath: ${apiPath}, queryTerm: ${queryTerm}`);
   const { data: movies } = useFetch(apiPath, queryTerm);
   useTitle(`search result for ${queryTerm} / Cinemate`);
-  
+
   return (
     <main>
       <section className="py-7">
         <p className="text-3xl text-gray-700 dark:text-white">
-          {movies.length == 0 ? `No Result found for ${queryTerm}` : `Result for ${queryTerm}`}
+          {movies.length === 0 ? `No Result found for ${queryTerm}` : `Result for ${queryTerm}`}
         </p>
       </section>
       <section className="max-w-7xl mx-auto py-7">
